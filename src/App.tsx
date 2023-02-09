4dc69f79c5e9a39f2bfd2cbfb9f18c8bd96fdb20
JAKEB MILBURN <MILBURNJ@UDEL.EDU>
2023-02-09 15:14:11 -0500

    added some stuff for html and css task

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from "react";
-import "./App.css";
-
+import "./App.css"; 
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 function App(): JSX.Element {
     return (
         <div className="App">
@@ -8,6 +10,24 @@ function App(): JSX.Element {
                 UD CISC275 with React Hooks and TypeScript
             </header>
             <p>Author: Jakeb Milburn - Hello World</p>
+            <header>
+                HTML and CSS task
+            </header>
+            <Container>
+            <Row>
+                <Col>
+                    List of things better than JavaScript
+                    <ul>
+                        <li>dirt</li>
+                        <li>taxes</li>
+                        <li>literally anything</li>
+                    </ul>
+                </Col>
+                <Col>
+                    Second column.
+                </Col>
+            </Row>
+            </Container>
         </div>
     );
 }
